36caa1c0a30df18efceb531e54f9b96e0a7216fd
Your Name <you@example.com>
2022-01-07 12:41:59 +0530

    New scope added

--- list234.py
+++ list234.py
@@ -3,14 +3,11 @@ print(str[-5]+str[-1])
 print(str[0:5:2])
 
 name="Geronimo"
-reverse=(name[0::-1])
+reverse=(name[0::-2])
 
-print(reverse) 
-print(name.lower())
-print(name.upper())
-print(name.count("g"))
-strs="hello beautiful wanna hold me "
-print(strs.replace("hold","AAHHHH"))
-print(strs.find("b"))
+for i in range(0,len(str)):
+    print(i,end="")
+    
+
 
 
