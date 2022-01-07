456aae21fafa6299d64cbb7cefa388bf25d6920c
Your Name <you@example.com>
2022-01-07 12:21:56 +0530

    version1

---
+++ list234.py
@@ -0,0 +1,16 @@
+str="uganda"
+print(str[-5]+str[-1])
+print(str[0:5:2])
+
+name="Geronimo"
+reverse=(name[0::-1])
+
+print(reverse) 
+print(name.lower())
+print(name.upper())
+print(name.count("g"))
+strs="hello beautiful wanna hold me "
+print(strs.replace("hold","AAHHHH"))
+print(strs.find("b"))
+
+
